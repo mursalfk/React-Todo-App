11e3b444c867d961de6793514c350582ee2a45d5
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2021-02-09 19:10:15 +0500

    removed free space

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ import './App.css';
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  
+  const [name, setName] = useState("Mursal");
+
+  const nameHandler = () => {
+    if (name == "Mursal"){
+      setName("Zohaad");
+    } else if (name == "Zohaad"){
+      setName("Mursal")
+    }
+    
+  }
+  
+  // State vs. Components vs. Props vs. Rerendering
+  
   return (
     <div className="App">
       {/* Header for all Pages, if more than one page */}
@@ -16,13 +30,26 @@ function App() {
 
       {/* Name can be used to show the logged in user */}
       <div className="App-header">
-        <Form 
-          inputText = {inputText}
-          todos = {todos} 
-          setTodos={setTodos} 
-          setInputText = {setInputText} />
-        <TodoList setTodos={setTodos} todos={todos}  />
-
+        <table>
+          <tr>
+            <td className="nameData">
+              {name}
+            </td>
+            <td>
+              <button onClick={nameHandler} className="shift">Shift</button>
+            </td>
+            <td>
+            <Form 
+              inputText = {inputText}
+              todos = {todos} 
+              setTodos={setTodos} 
+              setInputText = {setInputText} />
+            </td>
+            <td>
+              <TodoList setTodos={setTodos} todos={todos}  />
+            </td>
+          </tr>
+        </table>
       </div>
 
       {/* Footer for All pages */}
